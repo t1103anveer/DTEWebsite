ebe74623ba869137921cded508bd049a69022285
Tanveer <t1103anveer@hotmail.co.uk>
2019-11-08 22:32:44 +0000

    Completed home page html/css. Need to add images and a footer.

---
+++ src/OurTeam.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+export default function OurTeam() {
+
+    return(
+
+        <div>
+            <div className="ourTeam-wrapper">
+                <div className="title">
+                    <h1>Our Team</h1>
+                </div>
+
+                <div className="team-table">
+                    <div className="row">
+                        <div className="cardd col-lg-3 card1">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                        <div className="cardd col-lg-3 card2">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                        <div className="cardd col-lg-3 card3">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                        <div className="cardd col-lg-3 card4">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="cardd col-lg-3 card1">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                        <div className="cardd col-lg-3 card2">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                        <div className="cardd col-lg-3 card3">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                        <div className="cardd col-lg-3 card4">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="cardd col-lg-3 card1">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                        <div className="cardd col-lg-3 card2">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                        <div className="cardd col-lg-3 card3">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                        <div className="cardd col-lg-3 card4">
+                            <h5 className="team-name">Lorem Ipsum</h5>
+                            <p className="profile-title">Creative Designer</p>
+                            <a className="profile-link" href="#">View profile</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
