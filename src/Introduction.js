754981e21f68efdfaede1fb750506f3488cbd32c
Tanveer <t1103anveer@hotmail.co.uk>
2019-11-20 21:20:04 +0000

    Added background image

--- src/Introduction.js
+++ src/Introduction.js
@@ -13,6 +13,9 @@ export default function Introduction() {
                     <p>The Decarbonising Transport through Electrification (DTE) network represents a £1m EPSRC-funded multidisciplinary assembly aiming to address the challenges of implementing a low-carbon, cost-effective and holistically operating transportation sector for the UK. This will be facilitated through amalgamating research expertise from industry, academia and the public sector around the UK to identify and work towards solving the challenges limiting the current implementation of an electrified, synergistic transport system across the automotive, aerospace, maritime and rail sectors. Current research includes but is not limited to future electric vehicle charging infrastructure, electrifying facets of the aerospace sector through hybrid aircraft and electrification of the rail network. By leveraging further external funding, the network aims to be become self-sustainable for the foreseeable future and targets the establishment of an international conference. The network will be inclusive in its approach to research input and output. Mechanisms are in place to support early career researchers and dedicated work streams enable us to widen the dissemination of results. The outcome of the DTE network is expected to transform current practices relating to the decarbonisation of transport.</p>
                 </div>
             </div>
+            <div className="sponsors">
+
+            </div>
         </div>
     )
 }
